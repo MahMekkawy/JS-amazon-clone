58f23c57d418916f304bb2e95b0b425a4b54ff73
mahmoudmekkawy <mahmoud@yahoo.com>
2024-12-26 17:01:07 +0200

    Load products from the backend

--- scripts/main.js
+++ scripts/main.js
@@ -1,11 +1,18 @@
 import { cart, addToCart } from "../data/cart.js";
-import { products } from "../data/products.js";
+import { products, loadProducts } from "../data/products.js";
 import { formatCurrency } from "./utils/money.js";
 
-let productHTML = '';
 
-products.forEach((product) => {
-  productHTML += `
+
+loadProducts(renderProductsGrid);
+
+
+function renderProductsGrid() {
+
+  let productHTML = '';
+
+  products.forEach((product) => {
+    productHTML += `
         <div class="product-container">
         <div class="product-image-container">
           <img class="product-image" src="${product.image}">
@@ -55,29 +62,30 @@ products.forEach((product) => {
         </button>
       </div>
     `
-});
-
-document.querySelector('.products-grid').innerHTML = productHTML;
-
-// Make cart quantity interactive
-function updateCartQuantity() {
-  let cartQuantity = 0;
-
-  cart.forEach((cartItem) => {
-    cartQuantity += cartItem.quantity;
   });
 
-  document.querySelector('.js-cart-quantity').innerHTML = cartQuantity;
+  document.querySelector('.products-grid').innerHTML = productHTML;
 
+  // Make cart quantity interactive
+  function updateCartQuantity() {
+    let cartQuantity = 0;
+
+    cart.forEach((cartItem) => {
+      cartQuantity += cartItem.quantity;
+    });
+
+    document.querySelector('.js-cart-quantity').innerHTML = cartQuantity;
+
+  }
+
+  // Make Add to cart button interactive 
+  document.querySelectorAll('.js-add-to-cart').forEach((button) => {
+    button.addEventListener('click', () => {
+      const productId = button.dataset.productId;
+
+      addToCart(productId);
+      updateCartQuantity();
+
+    });
+  })
 }
-
-// Make Add to cart button interactive 
-document.querySelectorAll('.js-add-to-cart').forEach((button) => {
-  button.addEventListener('click', () => {
-    const productId = button.dataset.productId;
-
-    addToCart(productId);
-    updateCartQuantity();
-
-  });
-})
