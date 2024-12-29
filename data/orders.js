ee0e8530bb2b1dff4239070bddb674b44ea5428f
mahmoudmekkawy <mahmoud@yahoo.com>
2024-12-29 22:49:01 +0200

    Create order using backend

---
+++ data/orders.js
@@ -0,0 +1,10 @@
+export const orders = JSON.parse(localStorage.getItem('orders')) || [];
+
+export function addOrder(order) {
+    orders.unshift(order); // Using unshift so last order comes first
+    saveToStorage();
+}
+
+function saveToStorage() {
+    localStorage.setItem('orders', JSON.stringify(orders));
+}
